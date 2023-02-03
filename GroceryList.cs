91fa5e408e74d2f64a99ac0dc1c7ec7cd697a7ac
Emzzy241 <emzzyoluwole@gmail.com>
2023-02-03 10:18:26 +0100

    writing the C# code of my application

---
+++ GroceryList.cs
@@ -0,0 +1,23 @@
+using System;
+using System.collections.Generic;
+
+class GroceryList
+{
+    static void Main(){
+        // don't forget the Main is an entry point needed for a C# code to run
+
+        // YOU SHOULD ALWAYS NOTE: C# arrays would not accept more items in it after it has been created
+        // With Lists on the other hand you can add and Remove items from a List(with the .Add() and .Remove() methods)
+        // NOTE: Lists and arrays would never take in 2 different datatypes from you, they must always be the same... If you try it you get the error; cannot conver 'int' type to 'string'....
+        // all of this just basically re-explains why C# is a strongly-typed language
+
+        // back to our application now
+
+        // creating an array of string in C#
+        string[] theEntireArray = {"zero index", "first index", "second index", "third index"};
+
+        // looping through this C# array to print each individual item to 
+
+
+    }
+}
